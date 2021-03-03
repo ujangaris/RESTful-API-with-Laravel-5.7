72558e24b12fd2d0c1d9c2ca9cd954dbd00c2998
ujangaris <ujangaja@gmail.com>
2021-03-03 16:41:01 +0700

    03 Add a Record | country by id

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('country', 'Country\\CountryController@country');
+Route::get('country/{id}', 'Country\\CountryController@countryByID');
