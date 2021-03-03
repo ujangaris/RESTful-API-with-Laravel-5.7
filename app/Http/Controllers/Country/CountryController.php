8132de02ee0d96892a25da839690115dae26067f
ujangaris <ujangaja@gmail.com>
2021-03-03 17:46:34 +0700

    05 Validation | rules

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Country;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\CountryModel;
+use Validator;
 
 class CountryController extends Controller
 {
@@ -18,13 +19,22 @@ class CountryController extends Controller
         $country = CountryModel::find($id);
         if(is_null($country)){
 
-            return response()->json("Record not found!",404);
+            return response()->json(["message" => "Record not found!"],404);
         }
         return response()->json(CountryModel::find($id),200);
     }
 
     public function countrySave(Request $request)
     {
+        $rules = [
+            'name' => 'required|min:3',
+            'iso' => 'required|min:2|max:2',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
         $country = CountryModel::create($request->all());
         return response()->json($country,201);
     }
@@ -33,7 +43,7 @@ class CountryController extends Controller
     {
         $country = CountryModel::find($id);
         if(is_null($country)){
-            return response()->json("Record not found!", 404);
+            return response()->json(["message" => "Record not found!"], 404);
         }
         $country->update($request->all());
         return response()->json($country,200);
@@ -42,7 +52,7 @@ class CountryController extends Controller
     {
         $country = CountryModel::find($id);
         if (is_null($country)) {
-            return response()->json("Record not found!", 404);
+            return response()->json(["message" => "Record not found!"], 404);
         }
         $country->delete();
         return response()->json(null,204);
