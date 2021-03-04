912ad82d74bc4c27ab65205913600169004c4523
ujangaris <ujangaja@gmail.com>
2021-03-04 10:11:34 +0700

    10 OAuth2 Setup Passport

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
@@ -24,7 +25,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-
+        Passport::routes();
         parent::boot();
     }
 
